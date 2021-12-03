055f315982133267c3123a0985bea747c8b11119
Naka <ckaksj97@gmail.com>
2021-12-04 08:51:51 +0900

    loginCheck

--- src/main/java/com/blog/controller/LoginController.java
+++ src/main/java/com/blog/controller/LoginController.java
@@ -1,14 +1,15 @@
 package com.blog.controller;
 
 import com.blog.service.impl.loginServiceimpl;
+import com.blog.utils.ScriptUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @Controller
@@ -19,23 +20,27 @@ public class LoginController {
     public static loginServiceimpl ls = new loginServiceimpl();
     private boolean check = false;
 
-    @RequestMapping(value = "/login")
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String login() {
         return "login";
     }
 
-    @RequestMapping(value = "/login", method = {RequestMethod.GET, RequestMethod.POST})
-    public String login(HttpServletRequest request, HttpSession session) throws Exception {
+    @RequestMapping(value = "/loginCheck", method = RequestMethod.POST)
+    public String login(HttpServletRequest request, HttpServletResponse response, HttpSession session) throws Exception {
         request.setCharacterEncoding("utf-8");
         String userID = request.getParameter("userID");
         String userPassword = request.getParameter("userPassword");
+        if (session.getAttribute("userID") != null ){
+            session.removeAttribute("userID");
+        }
         var u = ls.login(userID, userPassword);
         if (u == 1) {
             session.setAttribute("userID", userID);
             return "redirect:main";
+        } else {
+            ScriptUtils.alertAndBackPage(response, "아이디또는 패스워드가 틀렸습니다.");
         }
-        check = true;
-        return "login";
+        return null;
     }
 
     @RequestMapping(value = "/register", method = RequestMethod.GET)
